5b9b902bb23604574f2015d8c71a230cd827f6b8
jhan403 <jmh082379@TouchScreenJH.>
2024-12-11 08:57:06 -0600

    Preliminary unworking code for project

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,10 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+def test_row_count(data):
+    '''Tests if the number of rows is bwtween 15000 and 1000000'''
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+    '''Tests if the price is between the min and max prices to determine if data is valid'''
+    return data['price'].between(min_price, max_price).all()
